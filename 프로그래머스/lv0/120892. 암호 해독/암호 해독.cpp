3ad1f4383501376138b6a41dba7a14e2824d4b55
Ryosel <52607507+Ryosel@users.noreply.github.com>
2023-08-02 22:47:00 +0900

    Update 암호 해독.cpp

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.cpp
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.cpp
@@ -1,16 +1,13 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
 string solution(string cipher, int code) {
     string answer = "";
     
-    
     for(int i = code - 1; i < cipher.size(); i += code){
         answer.push_back(cipher[i]);
-        //cout << cipher[i];
     }
     return answer;
 }
