071bdae82808bc0d959d4265eee660a8b942351e
Ryosel <52607507+Ryosel@users.noreply.github.com>
2023-08-06 21:43:53 +0900

    [unrated] Title: 공백으로 구분하기 1, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.cpp
+++ 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.cpp
@@ -1,6 +1,5 @@
 #include <string>
 #include <vector>
-#include <cctype>
 
 using namespace std;
 
@@ -11,7 +10,7 @@ vector<string> solution(string my_string) {
     size_t it = 0;
     while((it = my_string.find(delimiter)) != string::npos){
         answer.push_back(my_string.substr(0, it));
-        my_string.erase(0, it + delimiter.length());
+        my_string.erase(0, it + delimiter.size());
     }
     answer.push_back(my_string.substr(0, my_string.size()));
     return answer;
