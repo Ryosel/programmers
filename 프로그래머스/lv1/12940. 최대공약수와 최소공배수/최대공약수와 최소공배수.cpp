2a88232f97afb79e450c17766cd9eec3ab031f01
Ryosel <52607507+Ryosel@users.noreply.github.com>
2023-08-11 01:47:01 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
@@ -1,34 +1,18 @@
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
-int GCD(int n, int m){
-    int temp;
-    while(m != 0){
-        temp = n % m;
-        n = m;
-        m = temp;
-    }
-    return n;
-}
-
 vector<int> solution(int n, int m) {
     vector<int> answer;
-    int gcd = 1; //최대공약수
-    int lcm = 1; //최소공배수
-    
-    if(m % n == 0){
-        gcd = n;  
-        lcm = m;
-    }
-    else if(m % n != 0){
-        gcd = GCD(n, m);
-        lcm = ((n * m) / gcd);
-    }
+    int max = 0; //최대
+    int min = 0; //최소
 
-    answer.push_back(gcd);
-    answer.push_back(lcm);
-        
+    if(m % n == 0){ max = m; min = n; }
+    else if(m % n != 0){ max = m * n; min = 1; }
+    
+    answer.push_back(min);
+    answer.push_back(max);
     return answer;
 }
