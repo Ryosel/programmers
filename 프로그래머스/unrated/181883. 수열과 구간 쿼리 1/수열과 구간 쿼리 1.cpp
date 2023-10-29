268d68bcd9a58c0057da2f4db0b35c12fa2b915f
Ryosel <52607507+Ryosel@users.noreply.github.com>
2023-10-29 17:35:20 +0900

    [level unrated] Title: 수열과 구간 쿼리 1, Time: 0.23 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.cpp
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.cpp
@@ -1,12 +1,26 @@
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
-vector<int> solution(vector<int> arr, vector<vector<int>> queries) {  
-    for(int p = 0; p < queries.size(); p++){
-        for(int q = queries[p][0]; q <= queries[p][1]; q++) 
-            arr[q] += 1;
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    vector<int> answer;
+     vector<int> query;
+    
+    for(int i = 0; i <= arr.size(); i++){
+        for(int j = i; j < i+2; j++){
+            query.push_back(j);
+        }
+        //cout << query[i] << endl;
+        queries.push_back(query);
+        //query.clear();
     }
-    return arr;
+    
+    for(int k = 0; k < queries.size(); k++){
+        cout << queries[k][0];
+        cout << queries[k][1];
+        cout << "\n";
+    }
+    return answer;
 }
