736efd397d01ec78bfc022b162ff0a02d765de22
Eunhye Choi <52607507+Ryosel@users.noreply.github.com>
2025-03-25 00:18:25 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.cc
+++ 백준/Bronze/8393. 합/합.cc
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n = 0, sum = 0;
+int main(){
+    int n;
+    int sum = 0;
     cin >> n;
-    for(int i = 1; i <= n; i++){
+    for(int i=1; i<=n; i++){
         sum += i;
-    }
+    } 
     cout << sum;
+
     return 0;
 }
