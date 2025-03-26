bd9afecce073e17c3c02da935f5a42bcad0c806b
Eunhye Choi <52607507+Ryosel@users.noreply.github.com>
2025-03-27 00:57:49 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10998. A×B/A×B.cc
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -1,8 +1,9 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main(){
     int a, b;
-    scanf("%d %d", &a, &b);
-    printf("%d", a*b);
+    cin >> a >> b;
+    cout << a*b << endl;
     return 0;
 }
